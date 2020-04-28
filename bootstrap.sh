7838ad568da9cdcc527a1b1743dd9c6ccc684dd7
michaellee8 <ckmichael8@gmail.com>
2020-04-28 11:31:46 +0800

    refactor: remove unnecessary scripts

--- bootstrap.sh
+++
@@ -1,7 +0,0 @@
-sudo apt-get install curl unzip python2 \ 
-libglu1-mesa bash curl git unzip xz-utils zip \
-libvulkan1 mesa-vulkan-drivers
-
-cd fuchsia-cpp-gn-sdk && ./scripts/setup-and-test.sh && cd ../
-
-./flutter/bin/flutter precache --all-platforms --fuchsia
